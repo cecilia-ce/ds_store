bc1006741e4d6138ead73131e45a183ac2ca7d30
cecilia <cecilia.pb@hotmail.com>
2024-06-22 17:48:13 -0300

    feat: adiciona novos componentes na página de checkout

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -4,17 +4,36 @@
         <div class="d-flex ga-10 mt-16">
             <div>
                 <app-product />
-                <h1 class="my-10">Finalizando compra</h1>
+                <h1 class="my-8">Finalizando compra</h1>
+                <app-user-data-form />
+                <app-adress-form />
+                <app-payment-form />
             </div>
-            <app-summary />
+            <app-summary
+                :quantity=1 
+                :price="product.price"
+                :rating="product.rating"
+                :freightCharges="product.freightCharges"
+            />
+            
         </div>
     </main>
-
 </template>
+
+<script setup>
+    const product = {
+        id: 1,
+        price: 2450.99,
+        name: 'Telescópio Refletor Newtoniano 130mm Uranum Equatorial Astronômico Luneta',
+        rating: 4,
+        brand: 'Uranum',
+        freightCharges: 30
+    }   
+    
+</script>
 
 <style scoped>
 main {
-    color: #00FF95;;
+    color: #00FF95;
 }
-
 </style>
