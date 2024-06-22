55656f1f401f238dec51b0d3821a73ece69c0d15
cecilia <cecilia.pb@hotmail.com>
2024-06-22 17:47:33 -0300

    feat: adiciona componente AppUserDataForm

---
+++ components/AppUserDataForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" />
+        <h3 class="mb-5">Contato</h3>
+        <div class="d-flex ga-3">
+            <v-text-field label="Nome completo*"></v-text-field>
+            <v-text-field label="Telefone*"></v-text-field>
+        </div>
+        <v-text-field label="E-mail"></v-text-field>
+    </div>
+</template>
+
+
+<script setup>
+
+// Crie um formulário de dados pessoais: Nome completo, email (opcional) e telefone (obrigatório).
+
+</script>
