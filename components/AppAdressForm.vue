c534aff6d4346a6bdcb4a38649ca945522673303
cecilia <cecilia.pb@hotmail.com>
2024-06-22 17:47:17 -0300

    feat: adiciona componente AppAdressForm

---
+++ components/AppAdressForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <input type="text" />
+        <h3 class="mb-5">Endereço para entrega</h3>
+        <div class="d-flex ga-3">
+            <v-text-field label="CEP*"></v-text-field>
+            <v-text-field label="Endereço*"></v-text-field>
+        </div>
+        <div class="d-flex ga-3">
+            <v-text-field label="Número"></v-text-field>
+            <v-text-field label="Complemento"></v-text-field>
+            <v-text-field label="Cidade/UF"></v-text-field>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+// Crie um formulário de dados pessoais: Nome completo, email (opcional) e telefone (obrigatório).
+
+</script>
