6c8342f221bbd7be80e1ed6cdb7722aee79b835a
cecilia <cecilia.pb@hotmail.com>
2024-06-23 15:21:39 -0300

    feat: define emits e adiciona regras de campo obrigatório

--- components/AppUserDataForm.vue
+++ components/AppUserDataForm.vue
@@ -1,18 +1,37 @@
 <template>
+
     <div>
         <input type="text" />
         <h3 class="mb-5">Contato</h3>
         <div class="d-flex ga-3">
-            <v-text-field label="Nome completo*"></v-text-field>
-            <v-text-field label="Telefone*"></v-text-field>
-        </div>
-        <v-text-field label="E-mail"></v-text-field>
-    </div>
-</template>
+            <v-text-field 
+                label="Nome completo*" 
+                :rules="[requiredRule]"
+                @update:model-value="emitInput('name', $event)"
 
+            />
+            <v-text-field 
+                label="Telefone*" 
+                :rules="[requiredRule]" 
+                @update:model-value="emitInput('phone', $event)"
+            />
+        </div>
+        <v-text-field 
+            label="E-mail" 
+            @update:model-value="emitInput('email', $event)"
+            />
+    </div>
+
+</template>
 
 <script setup>
 
-// Crie um formulário de dados pessoais: Nome completo, email (opcional) e telefone (obrigatório).
+    const requiredRule = inputValue => !!inputValue || 'Este campo é obrigatório'
+
+    const emit = defineEmits(['updateUserData'])
+
+    const emitInput = (field, value) => {
+        emit('updateUserData', { field, value })
+    }
 
 </script>
